5168bb80975118323d3c952349a643bd5a15fc0e
mlipmanH <mlipman@college.harvard.edu>
2018-12-07 15:34:21 -0500

    got the finder working

--- reflector_finder.py
+++ reflector_finder.py
@@ -45,7 +45,7 @@ class Stream:
 
 def bin_is_refl(stream):
   """Checks if the stream begins with a reflector gadget opcode"""
-  if stream[0] is not 0xff:
+  if stream[0] != 0xff:
     return False
   bits = stream[1] & 0b00_111_000
   return (bits >= 0b00_010_000) and (bits <= 0b00_101_000)
@@ -53,9 +53,11 @@ def bin_is_refl(stream):
 def bin_is_call(stream):
   """Checks if the stream begins with a call of any type (for finding call-preceded gadgets)"""
   # print(chr(stream[0]) + chr(stream[1]))
-  if stream[0] is 0xe8:
+  if stream[0] == 0xe8:
     return True
-  elif stream[1] is 0xff:
+  elif stream[0] == 0x9a:
+    return True
+  elif stream[1] == 0xff:
     bits = stream[1] & 0b00_111_000
     return (bits >= 0b00_010_000) and (bits <= 0b00_011_000)
   else:
@@ -93,54 +95,65 @@ def last_with(p, stream):
 
 def objdump_string(stream):
   d = stream.baseaddr + stream.ix
+  # print(str(d))
   res = subprocess.run(['objdump', '-d', 
                         '--start-address=' + str(d), 
                         '--stop-address=' + str(d + 0x80),
                         stream.filename], stdout=subprocess.PIPE)
   return res.stdout.decode('utf-8')
 
-def objdump(s):
+def objdump(s, doprint=False):
   string = objdump_string(s)
-  ls = string.splitlines()
+  if doprint:
+    print(string)
+  ls = string.splitlines()[6:-1]
   return ls
 
 def obj_extract(l):
   r = re.compile("(?P<addr>.+):(?:\s+[0-9a-f]{2})+\s+(?P<instr>.+)")
-  return # TODO
+  return # TODO (currently dead code)
 
 def obj_is_refl(l):
-  '*' in l
+  return '*' in l
 
-def objdump_if_refl(s):
-  ob = objdump(s)
+def obj_is_ret(l):
+  return 'ret' in l
+
+def objdump_if_refl(s, t=False):
+  ob = objdump(s,t)
+  b = False
   for l in ob:
-    if obj_is_refl(l):
-      return ob
+    b = b or obj_is_refl(l)
+    if obj_is_ret(l):
+      return ob if b else None
   return None
 
 def get_all_cp_refls(s):
   objs = []
+  b = True
   while not s.killed:
     s = next_with(bin_is_call, s)
     if not s.killed:
       ob = objdump_if_refl(s)
       if ob is not None:
-        objs.push((s.ix, ob))
+        objs += [(s.ix, ob)]
+    b = False
   return objs
 
 def get_text_segment(f):
   res = subprocess.run(['objdump', '-j', '.text', '-s',
                         f], stdout=subprocess.PIPE)
-  out = res.stdout.decode('utf-8')
+  out = res.stdout.decode('ascii')
   bs = b''
   ls = out.splitlines()[4:-1]
-  off = int.from_bytes(bytes.fromhex(ls[0][1:7]), byteorder=sys.byteorder)
+  off = int(ls[0][1:8], 16)
+  # print(off)
   for l in ls:
     bs += extract_bytes(l)
   return Stream(bs, f, off)
 
 def extract_bytes(l):
-  ll = l[8:35].split(' ')
+  ll = l[8:44].split(' ')
   r = b''
   for w in ll:
     r += bytes.fromhex(w)
@@ -150,4 +163,4 @@ if __name__ == "__main__":
     s = get_text_segment(sys.argv[1])
     objs = get_all_cp_refls(s)
     for (addr, contents) in objs:
-        print(('0x%8x' % addr) + ":\n--------\n" + contents.join('\n'))
+        print(('0x%8x' % addr) + ":\n--------\n" + '\n'.join(contents))
